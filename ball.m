a1487d9d4bb5a423549fe4706e13099e4051380c
Flávio <della@belphegor.(none)>
2011-04-14 16:15:12 +0200

    commit 14/04

---
+++ ball.m
@@ -0,0 +1,7 @@
+function b = ball(m,n,r,M,N) 
+%create a ball at a specific spot and radius
+%  m, n: location,  r:radius, M, N: image size
+xc=n;yc=m;
+x = repmat(1:N, M,1);
+y = repmat((1:M)',1,N);
+b = (x-xc).^2+(y-yc).^2 <= r^2*ones(M,N);
