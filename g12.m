a04594ddcf90073b3f03912d7b5384959b0009c4
Flávio <della@belphegor.(none)>
2011-04-20 14:46:24 +0200

    Gaussian new implementatio

--- g12.m
+++ g12.m
@@ -4,11 +4,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function f = g12(n)
-f=double(zeros(n,n));
 A=1/sqrt(2*pi);
-xc=n/2; yc=n/2;
-for x=1:n
-    for y=1:n
-        f(x,y)=A*exp(-(y-yc)^2/2)*exp(-(x-xc)^2/2)*(x-xc)*(y-yc);
-    end
-end
+xc=(n)/2; yc=(n)/2;
+x = repmat(0:n-1, n,1);
+y = repmat((0:n-1)',1,n);
+f = A.*exp(-(y - yc).^2./2).*exp(-(x-xc).^2./2).*(x-xc).*(y-yc)
--- g22.m
+++ g22.m
@@ -3,11 +3,8 @@
 % this corresponds to entry i and j (i=2,j=2) in the Q matrix
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function f = g22(n)
-f=double(zeros(n,n));
 A=1/sqrt(2*pi);
-xc=n/2; yc=n/2;
-for x=1:n
-    for y=1:n
-        f(x,y)=A*exp(-(y-yc)^2/2)*exp(-(x-xc)^2/2)*((y-yc)^2-1);
-    end
-end
+xc=(n)/2; yc=(n)/2;
+x = repmat(0:n-1, n,1);
+y = repmat((0:n-1)',1,n);
+f=A.*exp(-(y-yc).^2./2).*exp(-(x-xc).^2./2).*((y-yc).^2-1);
