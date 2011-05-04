f109e4de217c635f7f918e104ac54e89aed127ed
Granha <granha1@gmail.com>
2011-05-04 15:28:53 +0200

    gauss fucntion with sigma parameter

--- g11.m
+++ g11.m
@@ -1,10 +1,12 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function f = g11(n, sigma)
 % second derivative of Gaussian along axis x 
 % this corresponds to entry i and j (i=1,j=1) in the Q matrix
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function f = g11(n)
-A=1/sqrt(2*pi);
+if nargin < 2
+  sigma = 1;
+end
+
+A=1/(sqrt(2*pi)*sigma);
 xc=(n)/2; yc=(n)/2;
 x = repmat(0:n-1, n,1);
 y = repmat((0:n-1)',1,n);
-f=A.*exp(-(y-yc).^2./2).*exp(-(x-xc).^2./2).*((x-xc).^2-1);
+f=A.*exp(-(y-yc).^2./(2*sigma^2)).*exp(-(x-xc).^2./(2*sigma^2)).*((x-xc).^2-1);
