9596c4cfe0eaf7512ea96d4304a6214963d8570d
Granha <granha1@gmail.com>
2011-05-04 15:32:09 +0200

    new image circle grid

--- main_response.m
+++ main_response.m
@@ -6,11 +6,12 @@ close all;
 %% parameters
 
 output_dir = '../results/';
-base_name = 'retina';
+base_name = 'rectangle';
 out_base = strcat(output_dir, base_name);
 
 noisy = 1;
 noisy_sigma = 0;
+pretreatement = 0;
 
 %% Create files
 
@@ -30,13 +31,23 @@ elseif strcmp(base_name, 'rectangle')
       Im(m,n) = 255;
     end
   end
+elseif strcmp(base_name, 'circle_grid')
+  R = 4; % circle radius
+  dist = 2*R+4; % distance between two adjancent circles
+  % draw a grid of circles
+  for m = (dist+4):dist:(M-(dist+4))
+    for n = (dist+4):dist:(N-(dist+4))
+      Im = Im + imsyn(N,[m,n],R); % combine circles to the previous image
+    end
+  end
 elseif strcmp(base_name, 'retina')
   Im = imread('../medical images/retina_origin.JPG');
   Im = rgb2gray(Im);
   Im = imcomplement(Im);
-  Im = imresize(Im, 1/4);
+%  Im = imresize(Im, 1/4);
+% Im = Im(14:134,41:130);
+  Im = Im(15:538,196:522);
   [M, N] = size(Im);
-  Im = Im(14:134,41:130);
 elseif strcmp(base_name, 'tjunc1') % 3D images
   load('../medical images/Tjunc1');
   [M,N,K] = size(Tjunc1);
@@ -49,9 +60,10 @@ end
 
 %% show image
 f_init = figure
-imshow(Im);
+%imshow(Im);
+image(Im); colormap(gray);
 title('original image');
-print(f_init, '-r80', '-depsc2', strcat(out_base, '_figure.eps'));
+print(f_init, '-r80', '-djpeg', strcat(out_base, '_figure.jpg'));
 
 %%  add noisy
 if noisy && noisy_sigma > 0
@@ -63,54 +75,69 @@ if noisy && noisy_sigma > 0
 end
 
 %% pretreatement
-if noisy
+if pretreatement
 %  Im = pretreatment(Im, 1);  
   Im = medfilt2(Im); % 3x3 neighborhoodx
   f_pretreated = figure
-  imshow(Im);
+  image(Im); colormap(gray);
+%  imshow(Im);
   title('pretreated image');
+  print(f_pretreated, '-r80', '-djpeg', strcat(out_base, '_pretreated.jpg'));  
 end
 
 % image has two value 0 background and the 
 % structure a constant near 255
 MAX = max(Im(:));
 
-[response r_matrix eigen_vectors_matrix eigen_values_matrix] = minimum_response(Im);
+[response r_matrix eigen_vectors_matrix eigen_values_matrix] = minimum_response(Im, 1, 8, 0.5, 1, 1, 0);
+
+[a b c d] = size(eigen_vectors_matrix)
 
 old_resp = response;
 %% remove noisy  by thresholding filter response
-if  noisy
+if  noisy && 0
   response = old_resp;
   threshold = 0.1*min(response(:)); % threshold: 5% of min
   idx = response > threshold;
-  response = imhmin(response,-threshold)
-  %response(idx) = 0;
+  response = imhmin(response,-threshold);
+  response(idx) = 0;
   r_matrix(idx) = 0; % remove noisy radius
   eigen_vectors_matrix(idx,:,:) = 0; % remove noisy vectors
   eigen_values_matrix(idx,:) = 0; % remove noisy eigen
                                            % values					 
-  %response = pretreatment(response, 1);  
+%  response = pretreatment(response, 1);  
 end
+
+[a b c d] = size(eigen_vectors_matrix)
 
 %% show 3D image of filtered response
 f_response = figure;
 surf(response);
 title('filter response');
-print(f_response, '-r80', '-depsc2', strcat(out_base, '_filtered.eps'));
+print(f_response, '-r80', '-djpeg', strcat(out_base, '_filtered.jpg'));
+
+
+% rescale radius valeus to better visualization
+min_radius = min(r_matrix(:));
+max_radius = max(r_matrix(:));
+aux_r_matrix = 40*(1/(max_radius-min_radius))*(r_matrix(:,:) - min_radius);
 
 f_radius = figure
-image(r_matrix); colormap(gray);
+image(aux_r_matrix); colormap(gray);
 title('radius');
-print(f_radius, '-r80', '-depsc2', strcat(out_base, '_radius.eps'));
+print(f_radius, '-r80', '-djpeg', strcat(out_base, '_radius.jpg'));
 
-direction = atan2(eigen_vectors_matrix(:,:,2,1),(eigen_vectors_matrix(:,:,1,1)+0.001));
+
+direction = atan2(eigen_vectors_matrix(:,:,2,1), eigen_vectors_matrix(:,:,1,1) + 0.0001);
+
 min_direction = min(direction(:));
 max_direction = max(direction(:));
-direction = 255*(1/(max_direction-min_direction))*(direction(:,:) - min_direction);
+direction = 80*(1/(max_direction-min_direction))*(direction(:,:) - min_direction);
+
 f_direction = figure
 image(direction); colormap(gray);
 title('vessel direction');
-print(f_direction, '-r80', '-depsc2', strcat(out_base, '_direction.eps'));
+print(f_direction, '-r80', '-djpeg', strcat(out_base, '_direction.jpg'));
 
 %% Find best threshold by comparing
 %% binarized image to the original one
@@ -134,7 +161,7 @@ print(f_direction, '-r80', '-depsc2', strcat(out_base, '_direction.eps'));
 % 
 % f_final = figure
 % image(reconstructed_im); colormap(gray);
-% print(f_final, '-r80', '-depsc2', strcat(out_base, '_reconstructed.eps'));
+% print(f_final, '-r80', '-djpeg', strcat(out_base, '_reconstructed.jpg'));
 % 
 % end
 
@@ -142,14 +169,14 @@ print(f_direction, '-r80', '-depsc2', strcat(out_base, '_direction.eps'));
 %%        Find local minimum
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%minimum = local_minimum(response, 0);
-minimum = imextendedmin(response, abs(0.3*min(min(response))));
+minimum = local_minimum(response);
+%minimum = imextendedmin(response, abs(0.3*min(min(response))));
 minimum_im = 255*minimum;
 
 f_minimum = figure
 image(minimum_im); colormap(gray);
 title('centerlines (local minima)');
-print(f_minimum, '-r80', '-depsc2', strcat(out_base, '_centerline.eps'));
+print(f_minimum, '-r80', '-djpeg', strcat(out_base, '_centerline.jpg'));
 
 
 %% Reconstructed image using centerline
@@ -158,8 +185,9 @@ print(f_minimum, '-r80', '-depsc2', strcat(out_base, '_centerline.eps'));
 
 reconstructed_im = 255*reconstruction(minimum, r_matrix);
 f_reconstructed = figure
-title('reconstructed vessels');
 image(reconstructed_im); colormap(gray);
+title('reconstructed vessels');
+print(f_reconstructed, '-r80', '-djpeg', strcat(out_base, '_reconstructed.jpg'));
 
 
 %%      Stochastic throwing balls
@@ -172,11 +200,11 @@ image(reconstructed_im); colormap(gray);
 %f_ball = figure
 %surf(ball_count);
 %title('balls stochastic');
-%print(f_ball, '-r80', '-depsc2', strcat(out_base, '_ball_stochastic.eps'));
+%print(f_ball, '-r80', '-djpeg', strcat(out_base, '_ball_stochastic.jpg'));
 %
 %% ball centerline
 %line = 255*(ball_count > 1.5*times*ones(M, N));
 %f_ball_centerline = figure;
 %image(line); colormap(gray);
 %title('ball centerline');
-%print(f_ball_centerline, '-r80', '-depsc2', strcat(out_base, '_ball_centerline.eps'));
+%print(f_ball_centerline, '-r80', '-djpeg', strcat(out_base, '_ball_centerline.jpg'));
